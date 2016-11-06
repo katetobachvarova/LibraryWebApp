09eb7c8a2fbce6fb72785e68c661ccbf29224318
katetobachvarova <kateto_bachvarova@abv.bg>
2016-11-07 01:49:04 +0200

    ADD: ItemRepository

---
+++ src/LibraryWebApp/Services/IItemRepository.cs
@@ -0,0 +1,20 @@
+﻿using LibraryWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryWebApp.Data
+{
+    public interface IItemRepository
+    {
+        Task<IEnumerable<Item>>SelectAll();
+        Task<Item> SelectByID(string id);
+        Task<Item> SelectByIDAsNoTracking(string id);
+        void Insert(Item obj);
+        void Update(Item obj);
+        Task Delete(string id);
+        Task Save();
+        //bug git
+    }
+}
---
+++ src/LibraryWebApp/Services/ItemRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryWebApp.Data
+{
+    public class ItemRepository : IItemRepository
+    {
+
+        private ApplicationDbContext context = null;
+
+
+        public ItemRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        
+        public async Task Delete(string id)
+        {
+            Item existing = await context.Items.SingleOrDefaultAsync(i => i.ItemId.ToString() == id);
+            context.Items.Remove(existing);
+        }
+
+        public void Insert(Item obj)
+        {
+            context.Items.Add(obj);
+        }
+
+        public async Task Save()
+        {
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Item>> SelectAll()
+        {
+            var allItems = await context.Items.Include(i => i.Title).Include(e => e.Title.Section).ToListAsync();
+            return allItems.AsEnumerable();
+        }
+
+        public async Task<Item> SelectByID(string id)
+        {
+            var item = await context.Items.Include(i => i.Title).SingleOrDefaultAsync(m => m.ItemId.ToString() == id);
+            return item;
+            
+        }
+
+        public async Task<Item> SelectByIDAsNoTracking(string id)
+        {
+            var item = await context.Items.AsNoTracking().Include(i => i.Title).SingleOrDefaultAsync(m => m.ItemId.ToString() == id);
+            return item;
+        }
+
+        public void Update(Item obj)
+        {
+            throw new NotImplementedException();
+        }
+        //bug git
+    }
+}
